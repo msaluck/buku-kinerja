36ae968be2d807e524a8e99992bec2d8153e85e4
Muhammad Syaiful Aliim <muhammad.syaiful.aliim@unsoed.ac.id>
2024-05-17 14:51:33 +0700

    update lagi

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,26 +11,26 @@ def dashboard_with_pivot(request):
     return render(request, "dashboard_with_pivot.html", {})
 
 
-def pivot_data(request):
-    dataset = FactMasaStudi.objects.select_related(
-        "fakultas",
-        "dim_jurusankode_jurusan",
-        "dim_prodikode_prodi",
-        "dim_tahun_akadkode_tahun_akad",
-        "dim_mahasiswanim",
-        "dim_statuskode_status",
-    ).all()
-    dataset = dataset[:1]
-    hasil_iterasi = []
-    for data in dataset:
-        nama_fakultas = data.fakultas.namafak
-
-    data = serializers.serialize("json", dataset)
-    print(data)
-    return JsonResponse(data, safe=False)
-
-
 # def pivot_data(request):
-# dataset = MasaStudiV2Kumulatif.objects.all()
-# data = serializers.serialize("json", dataset)
-# return JsonResponse(data, safe=False)
+#     dataset = FactMasaStudi.objects.select_related(
+#         "fakultas",
+#         "dim_jurusankode_jurusan",
+#         "dim_prodikode_prodi",
+#         "dim_tahun_akadkode_tahun_akad",
+#         "dim_mahasiswanim",
+#         "dim_statuskode_status",
+#     ).all()
+#     dataset = dataset[:1]
+#     hasil_iterasi = []
+#     for data in dataset:
+#         nama_fakultas = data.fakultas.namafak
+
+#     data = serializers.serialize("json", dataset)
+#     print(data)
+#     return JsonResponse(data, safe=False)
+
+
+def pivot_data(request):
+dataset = MasaStudiV2Kumulatif.objects.all()
+data = serializers.serialize("json", dataset)
+return JsonResponse(data, safe=False)
